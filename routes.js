9afcfcb669c229058af53aad84f00aea24f3f23e
Jedek <jeroen@jedeksvault.nl>
2016-01-04 18:19:20 +0100

    Added mobile friendly template

--- routes.js
+++ routes.js
@@ -5,19 +5,3 @@ Router.configure({
 Router.route('/', function () {
   	this.render('Home');
 });
-
-Router.route('/index.html', function () {
-  	this.render('Home');
-});
-
-Router.route('/basic_markup.html', function () {
-  	this.render('Markup');
-});
-
-Router.route('/single.html', function () {
-  	this.render('Single');
-});
-
-Router.route('/contact.html', function () {
-  	this.render('Contact');
-});
